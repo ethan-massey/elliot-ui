2ab73c31dbaa40a44159be48e95e6e7b2117eed4
Ethan Massey <ejm4aa@virginia.edu>
2023-06-23 11:02:31 -0400

    refactor try-catch

--- src/awsUtil.js
+++ src/awsUtil.js
@@ -1,17 +1,17 @@
 const { S3Client, ListObjectsV2Command } = require("@aws-sdk/client-s3")
 
 const getAudioFiles = async () => {
+  const client = new S3Client({
+    region: 'us-east-1',
+    credentials: {
+      accessKeyId: process.env.S3_ACCESS_KEY_ID,
+      secretAccessKey: process.env.S3_SECRET_ACCESS_KEY
+    }
+  });
+  const command = new ListObjectsV2Command({
+    Bucket: 'daily-elliot-audio'
+  });
   try {
-    const client = new S3Client({
-      region: 'us-east-1',
-      credentials: {
-        accessKeyId: process.env.S3_ACCESS_KEY_ID,
-        secretAccessKey: process.env.S3_SECRET_ACCESS_KEY
-      }
-    });
-    const command = new ListObjectsV2Command({
-      Bucket: 'daily-elliot-audio'
-    });
     const response = await client.send(command);
 
     const fileNames = response.Contents.map((item) => {
