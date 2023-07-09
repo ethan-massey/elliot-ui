373e91bfc5c3102daa71595f2549bd45dfe05a27
Ethan Massey <ejm4aa@virginia.edu>
2023-07-09 14:34:11 -0400

    add error handling

--- src/util/formatEpisodeData.js
+++ src/util/formatEpisodeData.js
@@ -4,26 +4,33 @@ const { getEpisodeSegmentsFromMongo } = require("./spotifyUtil");
 // returns formatted episode data for EJS views like so:
 // [{ title: formmattedDate, fileName: item, segments: [] }]
 const getFormattedEpisodeData = async () => {
-  var audioFiles = await getAudioFiles();
-  var episodeSegments = await getEpisodeSegmentsFromMongo();
 
   var episodes = [];
-  for (item of audioFiles) {
-    // get formatted date from filename to use as title
-    var formmattedDate = new Date(
-      `${item.substring(0, item.length - 13)} EST`
-    ).toDateString();
 
-    episodes.push({
-      title: formmattedDate,
-      fileName: item,
-      segments:
-        episodeSegments[item.substring(0, 10)] !== undefined
-          ? episodeSegments[item.substring(0, 10)].segments
-          : [],
-    });
+  try {
+    var audioFiles = await getAudioFiles();
+    var episodeSegments = await getEpisodeSegmentsFromMongo();
+
+    for (item of audioFiles) {
+      // get formatted date from filename to use as title
+      var formmattedDate = new Date(
+        `${item.substring(0, item.length - 13)} EST`
+      ).toDateString();
+  
+      episodes.push({
+        title: formmattedDate,
+        fileName: item,
+        segments:
+          episodeSegments[item.substring(0, 10)] !== undefined
+            ? episodeSegments[item.substring(0, 10)].segments
+            : [],
+      });
+    }
+    episodes.sort().reverse();
+    
+  } catch(err) {
+    console.log(err)
   }
-  episodes.sort().reverse();
 
   return episodes;
 };
